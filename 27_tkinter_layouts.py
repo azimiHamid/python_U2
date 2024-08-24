954faef1be4729ee830cf01310afc4fb834605e7
azimiHamid <azimihamid2003@gmail.com>
2024-08-24 11:27:03 +0500

    Add tkinter layouts and padding to the tkinter to the GUI elelements

--- 27_tkinter_layouts.py
+++ 27_tkinter_layouts.py
@@ -3,17 +3,18 @@ from tkinter import * # type: ignore
 window = Tk()
 window.title("My first GUI program")
 window.minsize(width=500, height=300)
+# Add padding
+window.config(padx=50, pady=50)
+
+
 
 # Label
-# Create a Label widget and use pack to place it in the window
 my_label = Label(text="I am a label", font=("Arial", 24, "italic"))
+# Tkinter Layouts
 my_label.pack()
-# my_label.pack(side="left")
-# Use pack with expand=1 to make the label fill any extra space in the window
-# my_label.pack(expand=1)
+my_label.place(x=0, y=0)
+my_label.grid(row=0, column=0)
 
-my_label["text"] = "New Text"
-my_label.config(text="Some Text")
 
 
 # Button
@@ -21,16 +22,14 @@ def button_clicked():
     my_label["text"] = input.get()
 
 button = Button(text="Click me", command=button_clicked)
-button.pack()
-
-button['text'] = "don't click"
-button.config(text='click here')
+button.grid(row=1, column=1)
+button.config(padx=10, pady=10)
 
 
 
 # Entry
 input = Entry(width=30)
-input.pack()
+input.grid(row=2, column=2)
 
 
 window.mainloop()
